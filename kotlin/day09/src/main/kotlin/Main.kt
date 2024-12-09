8b05c28d84f06ed99cabceaa9a3eb7bddde6aef9
Michael Gerhold <michael.gerhold@web.de>
2024-12-09 14:13:55 +0100

    use first solution after bugfix

--- kotlin/day09/src/main/kotlin/Main.kt
+++ kotlin/day09/src/main/kotlin/Main.kt
@@ -2,14 +2,15 @@ package org.example
 
 import kotlin.io.path.Path
 import kotlin.io.path.readText
+import kotlin.time.measureTime
 
 sealed class DiskBlock(val length: Int) {
     class Empty(length: Int) : DiskBlock(length) {
-        override fun toString() = "Empty($length)" //".".repeat(length)
+        override fun toString() = ".".repeat(length)
     }
 
     class File(length: Int, val id: Int) : DiskBlock(length) {
-        override fun toString() = "File(id=$id, length=$length)" //id.toString().repeat(length)
+        override fun toString() = id.toString().repeat(length)
     }
 }
 
@@ -75,64 +76,40 @@ fun part1(blocks: MutableList<DiskBlock>): ULong {
 }
 
 fun part2(blocks: MutableList<DiskBlock>): ULong {
-    @Suppress("NAME_SHADOWING")
-    var blocks = blocks
-
-    val lastFileId = (blocks.last() as DiskBlock.File).id
-
-    for (fileId in (0..lastFileId).reversed()) {
+    var nextFileId = (blocks.last() as DiskBlock.File).id
+    while (nextFileId > 0) {
         val fileIndex = blocks
             .withIndex()
-            .last { (_, block) -> block is DiskBlock.File && block.id == fileId }
+            .last { (_, block) -> block is DiskBlock.File && block.id == nextFileId }
             .index
-
-        val fileLength = blocks[fileIndex].length
-
         val gapIndex = blocks
             .take(fileIndex)
             .withIndex()
-            .firstOrNull { (_, block) -> block is DiskBlock.Empty && block.length >= fileLength }
+            .firstOrNull { (_, block) -> block is DiskBlock.Empty && block.length >= blocks[fileIndex].length }
             ?.index
-
         if (gapIndex == null) {
+            --nextFileId
             continue
         }
-
-        val file = blocks[fileIndex] as DiskBlock.File
-        blocks[fileIndex] = DiskBlock.Empty(fileLength)
         val gapLength = blocks[gapIndex].length
-        blocks[gapIndex] = file
-        if (fileLength < gapLength) {
+        val file = blocks.removeAt(fileIndex)
+        blocks.add(fileIndex, DiskBlock.Empty(file.length))
+
+        blocks.removeAt(gapIndex)
+        val fileLength = file.length
+        blocks.add(gapIndex, file)
+        if (gapLength > fileLength) {
             blocks.add(gapIndex + 1, DiskBlock.Empty(gapLength - fileLength))
         }
-
-        // Perform cleanup.
-        val newList = mutableListOf<DiskBlock>()
-        var gapAccumulator = 0
-        for (block in blocks) {
-            if (block is DiskBlock.Empty) {
-                gapAccumulator += block.length
-            } else {
-                if (gapAccumulator > 0) {
-                    newList.add(DiskBlock.Empty(gapAccumulator))
-                    gapAccumulator = 0
-                }
-                newList.add(block)
-            }
-        }
-
-        blocks = newList
+        --nextFileId
     }
 
     return blocks
         .fold(0UL to 0) { (checksum, index), block ->
             when (block) {
                 is DiskBlock.Empty -> checksum to index + block.length
-                is DiskBlock.File -> {
-                    val sum = (index..<index + block.length)
-                        .sumOf { it.toULong() * block.id.toULong() }
-                    checksum + sum to index + block.length
-                }
+                is DiskBlock.File -> checksum + (index..<index + block.length)
+                    .sumOf { it.toULong() * block.id.toULong() } to index + block.length
             }
         }
         .first
@@ -156,5 +133,9 @@ fun main() {
         .second
 
     println(part1(blocks.toMutableList()))
-    println(part2(blocks.toMutableList()))
+    println(
+        measureTime {
+            println(part2(blocks.toMutableList()))
+        }
+    )
 }
