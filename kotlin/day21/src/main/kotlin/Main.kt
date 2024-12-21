4bb9a957ad8dbb8b39f8387f03aaa400ff15629d
Michael Gerhold <michael.gerhold@web.de>
2024-12-21 15:13:51 +0100

    day 21 (Kotlin)

--- kotlin/day21/src/main/kotlin/Main.kt
+++ kotlin/day21/src/main/kotlin/Main.kt
@@ -3,6 +3,7 @@ package org.example
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
 import kotlin.math.abs
+import kotlin.math.min
 
 data class Vec2(val x: Int, val y: Int) {
     operator fun plus(other: Vec2) = Vec2(x + other.x, y + other.y)
@@ -37,7 +38,7 @@ enum class NumpadKey(val position: Vec2, val char: Char) {
     }
 }
 
-enum class DirectionalKey(val position: Vec2, val char: Char, val direction: Vec2) {
+enum class DirectionalKey(val position: Vec2, private val char: Char, val direction: Vec2) {
     /*     +---+---+
            | ^ | A |
        +---+---+---+
@@ -72,7 +73,7 @@ fun isValidNumpadSequence(sequence: List<DirectionalKey>): Boolean {
     return true
 }
 
-fun isValiDirectionalSequence(sequence: List<DirectionalKey>): Boolean {
+fun isValidDirectionalSequence(sequence: List<DirectionalKey>): Boolean {
     var current = DirectionalKey.A
     for (directionKey in sequence) {
         check(directionKey != DirectionalKey.FORBIDDEN)
@@ -109,17 +110,14 @@ fun numpadToDirectional(sequence: List<NumpadKey>): Sequence<List<DirectionalKey
     var result: Sequence<List<DirectionalKey>>? = null
     for (block in blocks) {
         val permutations = getPermutations(block.subList(0, block.size - 1)).map { it + block.last() }
-        if (result == null) {
-            result = permutations
-        } else {
-            result = result
-                .flatMap { a ->
-                    permutations
-                        .map { b ->
-                            a + b
-                        }
-                }
-        }
+        result = result
+            ?.flatMap { a ->
+                permutations
+                    .map { b ->
+                        a + b
+                    }
+            }
+            ?: permutations
     }
     check(result != null)
     return result.filter(::isValidNumpadSequence)
@@ -169,70 +167,50 @@ fun directionalToDirectional(sequence: List<DirectionalKey>): Sequence<List<Dire
     var result: Sequence<List<DirectionalKey>>? = null
     for (block in blocks) {
         val permutations = getPermutations(block.subList(0, block.size - 1)).map { it + block.last() }
-        if (result == null) {
-            result = permutations
-        } else {
-            result = result
-                .flatMap { a ->
-                    permutations
-                        .map { b ->
-                            a + b
-                        }
-                }
-        }
+        result = result
+            ?.flatMap { a ->
+                permutations
+                    .map { b ->
+                        a + b
+                    }
+            }
+            ?: permutations
     }
     check(result != null)
-    return result.filter(::isValiDirectionalSequence)
+    return result.filter(::isValidDirectionalSequence)
 }
 
-fun reverseDirectionalToDirectional(sequence: List<DirectionalKey>): List<DirectionalKey> {
-    var currentKey = DirectionalKey.A
-    val result = mutableListOf<DirectionalKey>()
-    for (inputKey in sequence) {
-        if (inputKey == DirectionalKey.A) {
-            result.add(currentKey)
-            continue
-        }
-        val delta = when (inputKey) {
-            DirectionalKey.UP -> Vec2(0, -1)
-            DirectionalKey.LEFT -> Vec2(-1, 0)
-            DirectionalKey.DOWN -> Vec2(0, 1)
-            DirectionalKey.RIGHT -> Vec2(1, 0)
-            else -> throw RuntimeException("Unreachable")
-        }
-        currentKey = DirectionalKey.entries.first { it.position == currentKey.position + delta }
-    }
-    return result
-}
+data class Params(val sequence: List<DirectionalKey>, val depth: Int)
 
-fun reverseNumericalToDirectional(sequence: List<DirectionalKey>): List<NumpadKey> {
-    var currentKey = NumpadKey.KEY_A
-    val result = mutableListOf<NumpadKey>()
-    for (inputKey in sequence) {
-        if (inputKey == DirectionalKey.A) {
-            result.add(currentKey)
-            continue
-        }
-        val delta = when (inputKey) {
-            DirectionalKey.UP -> Vec2(0, -1)
-            DirectionalKey.LEFT -> Vec2(-1, 0)
-            DirectionalKey.DOWN -> Vec2(0, 1)
-            DirectionalKey.RIGHT -> Vec2(1, 0)
-            else -> throw RuntimeException("Unreachable")
-        }
-        currentKey = NumpadKey.entries.first { it.position == currentKey.position + delta }
-    }
-    return result
-}
+val cache = mutableMapOf<Params, ULong>()
 
-fun findShortestLength(sequences: Sequence<List<DirectionalKey>>, depth: Int): Int {
+fun robot(sequence: List<DirectionalKey>, depth: Int): ULong {
     if (depth == 0) {
-        return sequences.first().size
+        return sequence.size.toULong()
     }
-    return sequences
-        .minOf { sequence ->
-            findShortestLength(directionalToDirectional(sequence), depth - 1)
+
+    val params = Params(sequence.toList(), depth)
+    if (params in cache) {
+        return cache[params]!!
+    }
+
+    val sequences = directionalToDirectional(sequence)
+    var min = ULong.MAX_VALUE
+    for (subSequence in sequences) {
+        var sum = 0UL
+        val part = mutableListOf<DirectionalKey>()
+        for (key in subSequence) {
+            part.add(key)
+            if (key == DirectionalKey.A) {
+                sum += robot(part, depth - 1)
+                part.clear()
+            }
         }
+        check(part.isEmpty())
+        min = min(min, sum)
+    }
+    cache[params] = min
+    return min
 }
 
 fun main() {
@@ -241,12 +219,16 @@ fun main() {
         .filter { !it.startsWith("//") }
         .map { it.substring(0, it.length - 1).toInt() to toNumpadSequence(it) }
 
-    var sum = 0
-    for ((numericValue, sequence) in sequences) {
-        val directionSequences1 = numpadToDirectional(sequence)
-        val shortestLength = findShortestLength(directionSequences1, 2)
-        println("$shortestLength * $numericValue = ${shortestLength * numericValue}")
-        sum += shortestLength * numericValue
-    }
-    println("sum: $sum")
+    arrayOf(2, 25)
+        .asSequence()
+        .withIndex()
+        .map { (i, numRobots) ->
+            i to sequences
+                .sumOf { (numericValue, sequence) ->
+                    numericValue.toULong() * numpadToDirectional(sequence)
+                        .map { robot(it, numRobots) }
+                        .min()
+                }
+        }
+        .forEach { (i, result) -> println("Part ${i + 1}: $result") }
 }
